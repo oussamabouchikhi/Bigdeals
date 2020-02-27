5c8496937d60f7ef891536de56dd99c7fceb7827
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-02-27 21:38:59 +0100

    Create Category model

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, ProductImage
+from product.models import ProductImage, Category
 
 admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
--- src/product/models.py
+++ src/product/models.py
@@ -10,14 +10,26 @@ class Product(models.Model):
     prodCreated = models.DateTimeField(verbose_name=_("Created at"))
     """
     because Product model is a Class each product is an object(instance)
-    so we need to define __str__ function to show he name of each product
+    so we need to define __str__ function to show the name of each product
     """
     def __str__(self):
         return self.prodName
 
-class ProductImage(object):
+class ProductImage(models.Model):
     # Every product has an image. delete image if product deleted
     PIProduct = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("Product"))
     PIImage = models.ImageField(upload_to='product/', verbose_name=_("Image"))
     def __str__(self):
         return str(self.PIProduct)
+
+class Category(models.Model):
+    CATName = models.CharField(max_length=50)
+    """
+    Main category & sub category are same so Parent references to itself. if there's no sub cate
+    it will be null
+    """
+    CATParent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    CATDesc = models.TextField()
+    CATImg = models.ImageField(upload_to='category/')
+    def __str__(self):
+        return str(self.CATName)
