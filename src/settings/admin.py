82424e08072bed2c70c956c16f3821a7b03b18b1
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-02-28 16:08:05 +0100

    Create Settings app

---
+++ src/settings/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Brand, Variant
+
+
+admin.site.register(Brand)
+admin.site.register(Variant)
---
+++ src/settings/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=40)
+    brand_desc = models.TextField(blank=True, null=True)
+
+    class Meta(object):
+        verbose_name = _("Brand")
+        verbose_name_plural = _("Brands")
+
+    def __str__(self):
+        return self.brand_name
+
+
+
+class Variant(models.Model):
+    variant_name = models.CharField(max_length=40)
+    variant_desc = models.TextField(blank=True, null=True)
+
+    class Meta(object):
+        verbose_name = _("Variant")
+        verbose_name_plural = _("Variants")
+
+    def __str__(self):
+        return self.brand_name
